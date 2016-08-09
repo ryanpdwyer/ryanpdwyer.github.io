14a4e9e4a1e9c0b0dfef19b3f27c6afca5290eec
Ryan Dwyer <ryanpdwyer@gmail.com>
2016-08-09 10:14:01 -0400

    Nikola auto commit.
    Nikola version: 7.7.11

--- conf.py
+++ conf.py
@@ -870,7 +870,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "ryanpdwyer"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
