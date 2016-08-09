66bf507da8f8fddc986443f50d42d4b1950b22e3
Ryan Dwyer <ryanpdwyer@gmail.com>
2016-08-09 09:54:04 -0400

    Nikola auto commit.
    Nikola version: 7.7.11

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Ryan Dwyer"  # (translatable)
 BLOG_TITLE = "My Nikola Site"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://ryanpdwyer.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
